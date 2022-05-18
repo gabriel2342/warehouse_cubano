286b93677251abac44c7379c81dbbd3dbbcdbf98
“gabriel2342” <gabriel.pendas@gmail.com>
2022-05-18 10:51:42 -0600

    Set item#index table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@ Warehouse.create(name: 'Habana')
 Warehouse.create(name: 'Holguin')
 Warehouse.create(name: 'Velasco')
 Warehouse.create(name: 'Santiago')
+Warehouse.create(name: 'Cienfuegos')
+
+
+Item.create(artist: 'Zilia Sanchez Dominguez', name: 'Maquinista, dipitico', quantity: 23, warehouse_id: 2 )
+Item.create(artist: 'Gina Pellon', name: 'Insulas al pairo', quantity: 23, warehouse_id: 5 )
+Item.create(artist: 'Sandra Ramos', name: 'La maldita circumstancia', quantity: 7, warehouse_id: 3 )
+Item.create(artist: 'Ana Albertina Delgado', name: 'Cosiendo mi corazon', quantity: 42, warehouse_id: 4 )
+Item.create(artist: 'Antonia Eiriz', name: 'Vigilia', quantity: 23, warehouse_id: 3 )
+Item.create(artist: 'Elsa Mora', name: 'Mirar al vacio', quantity: 7, warehouse_id: 1 )
+Item.create(artist: 'Kcho', name: 'Coluna infinita', quantity: 42, warehouse_id: 1 )
